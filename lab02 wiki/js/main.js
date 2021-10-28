17ae47e9b6eb0521fbf989f17ef464c9d660f4b1
Michael Clark <91759189+mclark123@users.noreply.github.com>
2021-10-27 23:23:29 -0700

    Add files via upload

---
+++ lab02 wiki/js/main.js
@@ -0,0 +1,23 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+function myFunction() {
+  // Get the checkbox
+  var checkBox = document.getElementById("myCheck");
+  // Get the output text
+  var text = document.getElementById("text");
+
+  // If the checkbox is checked, display the output text
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
